ef15ab44b4790a9fd3243da5c55c6df13ddf2267
Aadil <ansari.aadil863@gmail.com>
2020-06-14 23:03:30 +0530

    added repository interface

---
+++ src/main/java/com/project/repository/ContactsMasterRepo.java
@@ -0,0 +1,9 @@
+package com.project.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.entity.ContactsMaster;
+
+public interface ContactsMasterRepo extends JpaRepository<ContactsMaster, Integer>{
+
+}
