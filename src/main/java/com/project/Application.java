0c2ed155c7b44c341c3e2c313a87c89ce45c8ef5
Aadil <ansari.aadil863@gmail.com>
2020-06-14 22:54:28 +0530

    close ApplicationContext in main class all are done

--- src/main/java/com/project/Application.java
+++ src/main/java/com/project/Application.java
@@ -2,12 +2,15 @@ package com.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext config = SpringApplication.run(Application.class, args);
+		
+		config.close();
 	}
 
 }
